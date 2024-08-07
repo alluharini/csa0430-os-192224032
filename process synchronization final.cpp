94bd322f8aa378c2da3594bfc533e99e69fea484
Harini Allu <120156889+alluharini@users.noreply.github.com>
2024-08-07 09:45:29 +0530

    Add files via upload

---
+++ process synchronization final.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<pthread.h>
+#include<semaphore.h>
+sem_t sem;
+void* threadfunc(void* arg){
+	sem_wait(&sem);
+	printf("thread %d is in critical position\n",((int*)arg));
+	sem_post(&sem);
+	return NULL;
+}
+int main(){
+	pthread_t thread[3];
+	int ids[3]={1,2,3};
+	for(int i=0;i<3;i++)
+		pthread_create(&thread[i], NULL,threadfunc,&ids[i]);
+	for(int i=0;i<3;i++)
+		pthread_join(thread[i],NULL);
+	sem_destroy(&sem);
+	return 0;
+}
